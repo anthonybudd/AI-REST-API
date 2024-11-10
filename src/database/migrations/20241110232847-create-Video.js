5e91e6bd164fd139d16d5d07a175f064375278f6
Anthony C. Budd <anthonybudd94@gmail.com>
2024-11-10 17:28:52 -0600

    Migration Created

---
+++ src/database/migrations/20241110232847-create-Video.js
@@ -0,0 +1,13 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => queryInterface.createTable('Videos', {
+        id: {
+            type: Sequelize.UUID, defaultValue: Sequelize.UUIDV4, primaryKey: true, allowNull: false, unique: true }
+            , title: Sequelize.STRING, description: Sequelize.TEXT, url: Sequelize.STRING, uploadedAt: Sequelize.DATE, createdAt: {
+                type: Sequelize.DATE, allowNull: true }
+                , updatedAt: {
+                    type: Sequelize.DATE, allowNull: true }
+                    , deletedAt: {
+                        type: Sequelize.DATE, allowNull: true }
+                        , }
+                        ), down: (queryInterface, Sequelize) => queryInterface.dropTable('Videos') }
+                        ;
